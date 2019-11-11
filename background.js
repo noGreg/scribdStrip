8d8c7a5e8f4b5ecf8ff99412df07162883dc80b5
gregoryha <gregoryhernandeza@gmail.com>
2019-11-11 16:27:19 -0400

    messaging from background

--- background.js
+++ background.js
@@ -1,5 +1,9 @@
 chrome.browserAction.onClicked.addListener(clicked);
 
-function clicked(tab){
+function clicked(tab) {
     console.log('some clicked', tab);
+    let message = {
+        content: 'hello greg'
+    }
+    chrome.tabs.sendMessage(tab.id, message);
 }
--- content.js
+++ content.js
@@ -1,3 +1,7 @@
 (function(){
-    alert('something')
+    chrome.runtime.onMessage.addListener(receiveedMessage);
+
+    function receiveedMessage(message, sender, sendResponse) {
+        console.log('message: ', message, 'sender: ', sender, 'sendResponse: ', sendResponse);
+    }
 })()
