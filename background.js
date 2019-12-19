d30cf2f79028080d905e60d4bfe844eb4420994b
gregoryha <gregoryhernandeza@gmail.com>
2019-12-19 14:43:07 -0400

    First release almost done

--- background.js
+++ background.js
@@ -1,9 +1,2 @@
 chrome.browserAction.onClicked.addListener(clicked);
-
-function clicked(tab) {
-    console.log('some clicked', tab);
-    let message = {
-        content: 'hello greg'
-    }
-    chrome.tabs.sendMessage(tab.id, message);
-}
+function clicked(tab) { chrome.tabs.sendMessage(tab.id, { content: 'clean' }); }
--- content.js
+++ content.js
@@ -1,7 +1,40 @@
 (function(){
     chrome.runtime.onMessage.addListener(receiveedMessage);
-
     function receiveedMessage(message, sender, sendResponse) {
-        console.log('message: ', message, 'sender: ', sender, 'sendResponse: ', sendResponse);
+        if (message.content == 'clean') {
+            let pages = document.querySelectorAll('.outer_page');
+
+            Array.from(pages).forEach(page => {
+                try {
+                    let promo = page.querySelector('.auto__doc_page_webpack_doc_page_blur_promo');
+                    promo.parentElement.removeChild(promo);
+                    
+                    let blurWraps = page.querySelectorAll('[class^="ff"]');
+                    blurWraps.forEach(blurGroup => {
+                        Array.from(blurGroup.children).forEach(span => {
+                            Object.assign(span.style, {
+                                webkitTransition: 'all .5s',
+                                transition: 'all .5s'
+                            });
+                        });
+                        
+                        Array.from(blurGroup.children).forEach(span => {
+                            span.style.textShadow = 'black 0px 0px 1px';
+                        });
+                    });
+
+                    let blurImages = page.querySelectorAll('img');
+
+                    Array.from(blurImages).forEach(img => {
+                        Object.assign(img.style, {
+                            webkitTransition: 'all .5s',
+                            transition: 'all .5s'
+                        });
+                    });
+
+                    Array.from(blurImages).forEach(img => (img.style.opacity = 1));
+                } catch(err) {}
+            });
+        }
     }
-})()
+})();
