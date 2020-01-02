8a173e16499d4e4a61a0cdceb59a47e427aff6a3
noHard <gregory_hernandeza@hotmail.com>
2020-01-01 21:03:04 -0400

    A piece of documentation

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # scribdStrip
-Chrome script extension demostrating the Scribd page hidding system risk since simulate blur.
+
+Chrome extension for Scribd Document pages revealing without subscribing.
+
+# Use
+
+Just click the 'S' button on chrome bar to reveal the pages in front of you.
+
+![alt text](./demo.gif "Demo gif")
--- content.js
+++ content.js
@@ -1,13 +1,18 @@
 (function(){
     chrome.runtime.onMessage.addListener(receiveedMessage);
+
     function receiveedMessage(message, sender, sendResponse) {
         if (message.content == 'clean') {
             let pages = document.querySelectorAll('.outer_page');
 
             Array.from(pages).forEach(page => {
                 try {
+                    // remove promo pop-up from page
+
                     let promo = page.querySelector('.auto__doc_page_webpack_doc_page_blur_promo');
                     promo.parentElement.removeChild(promo);
+
+                    // Search for blured text and set to normal
                     
                     let blurWraps = page.querySelectorAll('[class^="ff"]');
                     blurWraps.forEach(blurGroup => {
@@ -23,8 +28,9 @@
                         });
                     });
 
-                    let blurImages = page.querySelectorAll('img');
+                    // Search for blured images and set to normal
 
+                    let blurImages = page.querySelectorAll('img');
                     Array.from(blurImages).forEach(img => {
                         Object.assign(img.style, {
                             webkitTransition: 'all .5s',
